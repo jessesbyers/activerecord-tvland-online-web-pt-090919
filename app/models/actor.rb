3e280762b6c4a9b70c02147c81409cb78f828d40
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-19 16:20:54 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    roles = []
+    self.characters.each do |char|
+      self.shows.each do |sh|
+      roles << "#{char.name} - #{sh.name}"
+      end
+    end
+    roles
+  end
+
 end
---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,12 @@
+class CreateCharacters < ActiveRecord::Migration[5.1]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
+    end
+  end
+end
+# A character should have a name, actor_id, and a show_id––
+# a character will belong to a show (the show migration is already provided) and an actor,
+# and we'll keep track of this relationship with these database table columns.
